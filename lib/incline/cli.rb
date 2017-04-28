3a1a9d004985f43080fb35e28e696d9077248f50
Beau Barker <beau@ckindustrial.com>
2017-04-28 10:57:18 -0400

    Prepare incline executable, but no functionality yet.

---
+++ lib/incline/cli.rb
@@ -0,0 +1,24 @@
+
+require 'incline/version'
+
+
+module Incline
+  ##
+  # The command line interface for Incline.
+  class Cli
+
+    ##
+    # Initializes an instance of the CLI with the specified parameters.
+    def initialize(*params)
+
+    end
+
+    ##
+    # Runs this instance of the CLI.
+    def run
+      puts 'This is only a placeholder for now.'
+    end
+
+  end
+end
+
