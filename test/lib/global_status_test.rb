51e2eb993cb9b447984cc2f1aa49bbb4dacb28cc
Beau Barker <beau@barkerest.com>
2017-04-28 22:01:23 -0400

    Add status tests.

--- test/lib/global_status_test.rb
+++ test/lib/global_status_test.rb
@@ -46,6 +46,44 @@ class GlobalStatusTest < ActiveSupport::TestCase
     end
   end
 
+  test 'should be able to set status' do
+    Incline::GlobalStatus::lock_for do |stat|
+      assert stat
 
+      # We haven't set a status, so the default messages should be in place.
+      # There should be no percentage yet either.
+      data = stat.get_status
+      assert data.is_a?(Hash)
+      assert data[:locked]
+      assert_equal 'The current process is busy.', data[:message]
+      assert data[:percent].blank?
+
+      # The helpers should match.
+      assert_equal data[:message], stat.get_message
+      assert_nil stat.get_percentage
+
+      # The message should be slightly different, but otherwise the data is the same.
+      data = Incline::GlobalStatus::current
+      assert data.is_a?(Hash)
+      assert data[:locked]
+      assert_equal 'The system is busy.', data[:message]
+      assert data[:percent].blank?
+
+      # Set the message.
+      stat.set_status 'Doing something.', 25
+
+      # The new status should be set correctly.
+      data = stat.get_status
+      assert_equal 'Doing something.', data[:message]
+      assert_equal '25', data[:percent]
+      assert_equal data[:message], stat.get_message
+      assert_equal 25, stat.get_percentage
+
+      # And this should be the same.
+      data = Incline::GlobalStatus::current
+      assert_equal 'Doing something.', data[:message]
+      assert_equal '25', data[:percent]
+    end
+  end
 
 end
