a927010c3b0b99420670be9e3f081b6133836f67
Beau Barker <beau@ckindustrial.com>
2017-06-23 11:22:10 -0400

    Add some tests.

--- test/models/incline/action_group_test.rb
+++ test/models/incline/action_group_test.rb
@@ -2,6 +2,25 @@ require 'test_helper'
 
 module Incline
   class ActionGroupTest < ActiveSupport::TestCase
-    # TODO: Add tests.
+    def setup
+      @item = Incline::ActionGroup.new(action_security: incline_action_securities(:one), access_group: incline_access_groups(:one))
+    end
+
+    test 'should be valid' do
+      assert @item.valid?
+    end
+
+    test 'should require action_security' do
+      assert_required @item, :action_security
+    end
+
+    test 'should require access_group' do
+      assert_required @item, :access_group
+    end
+
+    test 'access_group should be unique within action_security' do
+      assert_uniqueness @item, :access_group, action_security: incline_action_securities(:two)
+    end
+
   end
 end
