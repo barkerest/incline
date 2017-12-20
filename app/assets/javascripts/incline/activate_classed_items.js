2c6dddcd69b0fdec25aeaa85917e465cf18ebfc1
Beau Barker <beau@barkerest.com>
2017-12-20 12:50:48 -0500

    Fix select2 activation.

--- app/assets/javascripts/incline/activate_classed_items.js
+++ app/assets/javascripts/incline/activate_classed_items.js
@@ -1,11 +1,17 @@
 
 function activateClassedItems(base) {
-    if (base === undefined) base = $(document);
+	var dropbase;
+    if (base === undefined){
+		base = $(document);
+		dropbase = $('body');
+	}else{
+		dropbase = base;
+	}
     base.find('div.date').datepicker({ format: "mm/dd/yyyy", todayBtn: "linked", todayHighlight: true, clearBtn: true });
     base.find('input.float').regexMask('float');
     base.find('input.integer').regexMask('integer');
     base.find('.double-scroll').doubleScroll({ onlyIfScroll: true, resetOnWindowResize: true });
-    base.find('select.select2').select2();
+    base.find('select.select2').select2(dropdownParent: base);
     base.find('a.inline_action').inlineAction();
     base.find('a.inline_form').inlineForm();
 }
